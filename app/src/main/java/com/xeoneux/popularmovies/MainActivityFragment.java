f6ef7df9b85ac37acad655360a38503ac085207b
Aayush Kapoor <xeoneux@icloud.com>
2016-03-03 20:36:27 +0530

    Display images in GridView

--- app/src/main/java/com/xeoneux/popularmovies/MainActivityFragment.java
+++ app/src/main/java/com/xeoneux/popularmovies/MainActivityFragment.java
@@ -1,5 +1,6 @@
 package com.xeoneux.popularmovies;
 
+import android.content.Context;
 import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -7,7 +8,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
@@ -21,13 +26,36 @@ public class MainActivityFragment extends Fragment {
     public MainActivityFragment() {
     }
 
+    public static String[] imageUrls = {
+            "http://lorempixel.com/492/492/abstract",
+            "http://lorempixel.com/492/492/animals",
+            "http://lorempixel.com/492/492/business",
+            "http://lorempixel.com/492/492/cats",
+            "http://lorempixel.com/492/492/city",
+            "http://lorempixel.com/492/492/food",
+            "http://lorempixel.com/492/492/nightlife",
+            "http://lorempixel.com/492/492/fashion",
+            "http://lorempixel.com/492/492/people",
+            "http://lorempixel.com/492/492/nature",
+            "http://lorempixel.com/492/492/sports",
+            "http://lorempixel.com/492/492/technics",
+            "http://lorempixel.com/492/492/transport"
+    };
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        GridView gridView = (GridView) rootView.findViewById(R.id.grid_view);
+        gridView.setAdapter(new ImageAdapter(getContext(), imageUrls));
+        return rootView;
+    }
+
+    @Override
+    public void onStart() {
         FetchMoviesTask fetchMoviesTask = new FetchMoviesTask();
         fetchMoviesTask.execute("popularity");
-        return rootView;
+        super.onStart();
     }
 
     public class FetchMoviesTask {
@@ -85,6 +113,49 @@ public class MainActivityFragment extends Fragment {
             return builder.build().toString();
 
         }
+
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+
+        private Context context;
+        private String[] imageUrls;
+
+        public ImageAdapter(Context context, String[] imageUrls) {
+            this.context = context;
+            this.imageUrls = imageUrls;
+        }
+
+        public int getCount() {
+            return imageUrls.length;
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if (convertView == null) {
+                imageView = new ImageView(context);
+                imageView.setLayoutParams(new GridView.LayoutParams(492, 492));
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+            Glide
+                    .with(context)
+                    .load(imageUrls[position])
+                    .centerCrop()
+                    .into(imageView);
+
+            return imageView;
+        }
+
 
     }
 
