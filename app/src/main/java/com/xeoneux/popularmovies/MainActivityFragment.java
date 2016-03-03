50a045a843e0e3553ed623f2c0132f306c4910dc
Aayush Kapoor <xeoneux@icloud.com>
2016-03-03 16:28:58 +0530

    Fetch JSON data from The Movie Database

--- app/src/main/java/com/xeoneux/popularmovies/MainActivityFragment.java
+++ app/src/main/java/com/xeoneux/popularmovies/MainActivityFragment.java
@@ -1,10 +1,17 @@
 package com.xeoneux.popularmovies;
 
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import cz.msebera.android.httpclient.Header;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +24,68 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        FetchMoviesTask fetchMoviesTask = new FetchMoviesTask();
+        fetchMoviesTask.execute("popularity");
+        return rootView;
     }
+
+    public class FetchMoviesTask {
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
+
+        public void execute(String sortType) {
+
+            String URL = buildURL(sortType);
+
+            client.get(URL, new AsyncHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                    String response = new String(responseBody);
+                    Log.d(LOG_TAG, response);
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                    Log.e(LOG_TAG, error.getMessage());
+                }
+            });
+
+        }
+
+        private String buildURL(String sortType) {
+
+            String sortParam;
+
+            switch (sortType) {
+                case "popularity":
+                    sortParam = "popularity.desc";
+                    break;
+                case "rating":
+                    sortParam = "vote_average.desc";
+                    break;
+                default:
+                    sortParam = "popularity.desc";
+            }
+
+            String APIVersion = "3";
+            String APIKey = BuildConfig.THE_MOVIE_DATABASE_API_KEY;
+
+            Uri.Builder builder = new Uri.Builder();
+            builder.scheme("https")
+                    .authority("api.themoviedb.org")
+                    .appendPath(APIVersion)
+                    .appendPath("discover")
+                    .appendPath("movie")
+                    .appendQueryParameter("api_key", APIKey)
+                    .appendQueryParameter("sort_by", sortParam);
+
+            return builder.build().toString();
+
+        }
+
+    }
+
 }
